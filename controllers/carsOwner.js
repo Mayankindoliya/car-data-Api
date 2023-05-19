aad7e6831382f99a7eee8bf2d7ef9a07dd77c1b8
Mayank <mayankindoliya@gmail.com>
2023-05-19 17:55:09 +0530

    added a conditon in registerCarsOwners controller.

--- controllers/carsOwner.js
+++ controllers/carsOwner.js
@@ -3,13 +3,20 @@ const Cars = require('../models/cars');
 const bcrypt = require('bcrypt');
 
 class carsOwnerControllers {
+
   static async registerCarsOwner(document) {
+   const existingOwner = await Cars.findOne({$or: [{emailId: document.emailId}, {ownerName: document.ownerName}]}).lean()
+   if(existingOwner) {
+    throw new Error("Owner Already Exists?")
+   }
     const salt = bcrypt.genSaltSync(10)
     const hash = bcrypt.hashSync(document.password, salt)
     document.password = hash
     const cars = await Cars.create(document)
     return cars
   }
+
+  static async 
 };
 
 module.exports = carsOwnerControllers;
